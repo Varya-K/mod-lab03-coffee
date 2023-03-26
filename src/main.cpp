25e8771ba2127103c6c4430f7a52f15e8c0d77a2
Varya-K <126766019+Varya-K@users.noreply.github.com>
2023-03-26 06:33:09 +0300

    Add Automata.cpp and main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+﻿#include "Automata.h";
+#include <iostream>
+
+int main()
+{
+    std::vector <std::string> Menu = {"Espresso", "Cappuccino", "Americano", "Macchiato", "Mocha", "Latte", "Flat White", "Raf"};
+    std::vector <int> Prices = { 120, 200, 180, 220, 210, 160, 250, 230 };
+    Automata a = Automata(Menu,Prices);
+    a.on();
+    a.coin(100);
+    a.coin(100);
+    a.choice(4);
+    a.coin(200);
+    a.coin(50);
+    a.choice(4);
+    a.cook();
+    a.finish();
+    a.off();
+    
+}
