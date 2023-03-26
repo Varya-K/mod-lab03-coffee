cd0251ce610f67657a95775deb8a08399844a0b8
Varya-K <126766019+Varya-K@users.noreply.github.com>
2023-03-26 11:43:37 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
+// Copyright 2022 UNN-IASR
 #include "Automata.h"
 #include <iostream>
 
-int main(){
-    std::vector <std::string> Menu = {"Espresso", 
-                                      "Cappuccino", "Americano", "Macchiato", 
-                                      "Mocha", "Latte", "Flat White", "Raf"};
-    std::vector <int> Prices = { 120, 200, 
-                                180, 220, 210, 160, 250, 230 };
+int main() {
+    std::vector <std::string> Menu = { "Espresso",
+                                       "Cappuccino", "Americano", "Macchiato",
+                                       "Mocha", "Latte", "Flat White", "Raf" };
+    std::vector <int> Prices = { 120, 200,
+                                 180, 220, 210, 160, 250, 230 };
     Automata a = Automata(Menu, Prices);
     a.on();
     a.coin(100);
