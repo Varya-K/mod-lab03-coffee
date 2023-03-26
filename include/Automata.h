e75c15d9b6413ddd861ceaa3227d0fe48fd79757
Varya-K <126766019+Varya-K@users.noreply.github.com>
2023-03-26 06:25:31 +0300

    Add Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+#include <vector>
+
+
+enum STATES { OFF, WAIT, ACCEPT, CHECK, COOK };
+
+class Automata {
+private:
+	int cash;
+	std::vector <std::string> menu = { "Espresso", "Cappuccino", "Americano", "Macchiato", "Mocha", "Latte", "Flat White" };
+	std::vector <int> prices = {120, 200, 180, 220, 210, 160, 250};
+	STATES state;
+	int num_of_chousen_drink;
+	void check();
+public:
+	Automata();
+	Automata(std::vector <std::string> Menu, std::vector <int> Prices);
+	void on();
+	void off();
+	void coin(int money);
+	void getMenu();
+	STATES getState();
+	void choice(int num_of_choice);
+	void cook();
+	void finish();
+	void cancel();
+};
